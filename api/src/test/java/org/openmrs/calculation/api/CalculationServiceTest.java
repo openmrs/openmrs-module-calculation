efd239ec362f4e7781c7b11aff72d2cf34cef6fd
Wyclif Luyima <wyclif@openmrs.org>
2012-02-15 04:16:01 +0000

    Added unit tests for CRUD operations for token registrations

--- api/src/test/java/org/openmrs/calculation/api/CalculationServiceTest.java
+++ api/src/test/java/org/openmrs/calculation/api/CalculationServiceTest.java
@@ -13,9 +13,14 @@
  */
 package org.openmrs.calculation.api;
 
+import junit.framework.Assert;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.openmrs.api.context.Context;
+import org.openmrs.calculation.TokenRegistration;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.openmrs.test.Verifies;
 
 /**
  * Contains test methods for {@link CalculationService}
@@ -24,8 +29,82 @@ public class CalculationServiceTest extends BaseModuleContextSensitiveTest {
 	
 	private CalculationService service;
 	
+	private static final String TOKEN_UUID = "467dd0d8-5785-11e1-80a0-00248140a5eb";
+	
 	@Before
 	public void before() {
 		service = Context.getService(CalculationService.class);
+	}
+	
+	/**
+	 * @see {@link CalculationService#getTokenRegistration(Integer)}
+	 */
+	@Test
+	@Verifies(value = "should return a token with a matching id", method = "getTokenRegistration(Integer)")
+	public void getTokenRegistration_shouldReturnATokenWithAMatchingId() throws Exception {
+		Assert.assertEquals(TOKEN_UUID, service.getTokenRegistration(1).getUuid());
+	}
+	
+	/**
+	 * @see {@link CalculationService#getTokenRegistrationByName(String)}
+	 */
+	@Test
+	@Verifies(value = "should fetch a token with a matching name", method = "getTokenRegistrationByName(String)")
+	public void getTokenRegistrationByName_shouldFetchATokenWithAMatchingName() throws Exception {
+		Assert.assertEquals(TOKEN_UUID, service.getTokenRegistrationByName("gender").getUuid());
+	}
+	
+	/**
+	 * @see {@link CalculationService#getTokenRegistrationByUuid(Integer)}
+	 */
+	@Test
+	@Verifies(value = "should fetch a token with a matching uuid", method = "getTokenRegistrationByUuid(Integer)")
+	public void getTokenRegistrationByUuid_shouldFetchATokenWithAMatchingUuid() throws Exception {
+		Assert.assertEquals("gender", service.getTokenRegistrationByUuid(TOKEN_UUID).getName());
+	}
+	
+	/**
+	 * @see {@link CalculationService#getAllTokenRegistrations()}
+	 */
+	@Test
+	@Verifies(value = "should get all tokens in the database", method = "getAllTokenRegistrations()")
+	public void getAllTokenRegistrations_shouldGetAllTokensInTheDatabase() throws Exception {
+		Assert.assertEquals(6, service.getAllTokenRegistrations().size());
+	}
+	
+	/**
+	 * @see {@link CalculationService#saveTokenRegistration(TokenRegistration)}
+	 */
+	@Test
+	@Verifies(value = "should save the specified tokenRegistration to the database", method = "saveTokenRegistration(TokenRegistration)")
+	public void saveTokenRegistration_shouldSaveTheSpecifiedTokenRegistrationToTheDatabase() throws Exception {
+		int originalTokenCount = service.getAllTokenRegistrations().size();
+		TokenRegistration token = new TokenRegistration();
+		token.setName("test token registration");
+		token.setProviderClassName("test.provider.Class");
+		service.saveTokenRegistration(token);
+		Assert.assertNotNull(token.getTokenRegistrationId());
+		Assert.assertEquals(originalTokenCount + 1, service.getAllTokenRegistrations().size());
+	}
+	
+	/**
+	 * @see {@link CalculationService#purgeTokenRegistration(TokenRegistration)}
+	 */
+	@Test
+	@Verifies(value = "should purge the specified tokenRegistration from the database", method = "purgeTokenRegistration(TokenRegistration)")
+	public void purgeTokenRegistration_shouldPurgeTheSpecifiedTokenRegistrationFromTheDatabase() throws Exception {
+		TokenRegistration token = service.getTokenRegistrationByUuid(TOKEN_UUID);
+		Assert.assertNotNull(service.getTokenRegistrationByUuid(TOKEN_UUID));//control check
+		service.purgeTokenRegistration(token);
+		Assert.assertNull(service.getTokenRegistrationByUuid(TOKEN_UUID));
+	}
+	
+	/**
+	 * @see {@link CalculationService#findTokens(String)}
+	 */
+	@Test
+	@Verifies(value = "should get all tokenRegistrations with a matching name", method = "findTokens(String)")
+	public void findTokens_shouldGetAllTokenRegistrationsWithAMatchingName() throws Exception {
+		Assert.assertEquals(5, service.findTokens("name").size());
 	}
 }
