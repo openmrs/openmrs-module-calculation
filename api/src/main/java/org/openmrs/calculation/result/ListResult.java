5a62e721373d9f6abbbd84ce816b642b6c89f162
Daniel Kayiwa <dkayiwa@openmrs.org>
2012-03-13 21:18:29 +0000

    Simplify adding/removing of results to/from a ListResult - CALC-2

--- api/src/main/java/org/openmrs/calculation/result/ListResult.java
+++ api/src/main/java/org/openmrs/calculation/result/ListResult.java
@@ -13,25 +13,26 @@
  */
 package org.openmrs.calculation.result;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openmrs.calculation.api.patient.PatientCalculationContext;
 import org.openmrs.calculation.patient.PatientCalculation;
 
 /**
- * A {@link Result} backed by a {@link List} of results with convenience methods that get the first
- * and last results in the backing list
+ * A {@link Result} backed by a {@link List} of results with convenience methods
+ * that get the first and last results in the backing list
  */
 public class ListResult implements Result {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private PatientCalculationContext calculationContext;
-	
+
 	private PatientCalculation calculation;
-	
+
 	private List<Result> results;
-	
+
 	/**
 	 * @see org.openmrs.calculation.result.Result#getCalculation()
 	 */
@@ -39,14 +40,15 @@ public class ListResult implements Result {
 	public PatientCalculation getCalculation() {
 		return calculation;
 	}
-	
+
 	/**
-	 * @param calculation the calculation to set
+	 * @param calculation
+	 *            the calculation to set
 	 */
 	public void setCalculation(PatientCalculation calculation) {
 		this.calculation = calculation;
 	}
-	
+
 	/**
 	 * @see org.openmrs.calculation.result.Result#getCalculationContext()
 	 */
@@ -54,14 +56,16 @@ public class ListResult implements Result {
 	public PatientCalculationContext getCalculationContext() {
 		return calculationContext;
 	}
-	
+
 	/**
-	 * @param calculationContext the calculationContext to set
+	 * @param calculationContext
+	 *            the calculationContext to set
 	 */
-	public void setCalculationContext(PatientCalculationContext calculationContext) {
+	public void setCalculationContext(
+			PatientCalculationContext calculationContext) {
 		this.calculationContext = calculationContext;
 	}
-	
+
 	/**
 	 * @see org.openmrs.calculation.result.Result#getValue()
 	 */
@@ -69,14 +73,15 @@ public class ListResult implements Result {
 	public Object getValue() {
 		return results;
 	}
-	
+
 	/**
-	 * @param value the value to set
+	 * @param value
+	 *            the value to set
 	 */
 	public void setValue(List<Result> value) {
 		this.results = value;
 	}
-	
+
 	/**
 	 * @see org.openmrs.calculation.result.Result#isEmpty()
 	 */
@@ -84,7 +89,7 @@ public class ListResult implements Result {
 	public boolean isEmpty() {
 		return results == null || results.isEmpty();
 	}
-	
+
 	/**
 	 * @see org.openmrs.calculation.result.Result#asType(java.lang.Class)
 	 */
@@ -92,7 +97,7 @@ public class ListResult implements Result {
 	public <T> T asType(Class<T> clazz) {
 		return ResultUtil.convert(this, clazz);
 	}
-	
+
 	/**
 	 * Gets the first result in the backing list
 	 * 
@@ -101,7 +106,7 @@ public class ListResult implements Result {
 	public Result getFirstResult() {
 		return isEmpty() ? new EmptyResult() : results.get(0);
 	}
-	
+
 	/**
 	 * Gets the last result in the backing list
 	 * 
@@ -109,5 +114,52 @@ public class ListResult implements Result {
 	 */
 	public Result getLastResult() {
 		return isEmpty() ? new EmptyResult() : results.get(results.size() - 1);
+	}
+
+	/**
+	 * Adds a result to the list
+	 * 
+	 * @param result
+	 *            the result to add
+	 * @return the added {@link Result}
+	 */
+	public Result add(Result result) {
+		if (results == null) {
+			results = new ArrayList<Result>();
+		}
+
+		results.add(result);
+
+		return result;
+	}
+
+	/**
+	 * Removes a given result from the list
+	 * 
+	 * @param result
+	 *            the result to remove
+	 * @return true if this list contained the specified result and it was removed
+	 */
+	public boolean remove(Result result) {
+		if (isEmpty()) {
+			return false;
+		}
+
+		return results.remove(result);
+	}
+
+	/**
+	 * Removes the result at a given position in the list
+	 * 
+	 * @param index
+	 *            the position
+	 * @return the removed {@link Result}
+	 */
+	public Result remove(int index) {
+		if (isEmpty()) {
+			return null;
+		}
+
+		return results.remove(index);
 	}
 }
