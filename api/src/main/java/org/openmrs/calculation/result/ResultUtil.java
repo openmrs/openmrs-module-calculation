e7b7d1ecef5209819be4a353879bee7a4e67a257
Darius Jazayeri <jazayeri@alum.mit.edu>
2012-07-26 06:34:03 +0000

    ResultUtil.convert and CalculationUtil.cast do not correctly convert things to Boolean - CALC-43

--- api/src/main/java/org/openmrs/calculation/result/ResultUtil.java
+++ api/src/main/java/org/openmrs/calculation/result/ResultUtil.java
@@ -13,6 +13,8 @@
  */
 package org.openmrs.calculation.result;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -21,8 +23,11 @@ import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Concept;
+import org.openmrs.Obs;
 import org.openmrs.calculation.CalculationUtil;
 import org.openmrs.calculation.ConversionException;
+import org.openmrs.util.OpenmrsConstants;
 
 /**
  * Contains utility methods to handle {@link CalculationResult}s
@@ -61,11 +66,17 @@ public class ResultUtil {
 	 * @should return an empty collection if the result has a null value and class is a list
 	 * @should return an empty collection if the result is null and class is a set
 	 * @should return an empty collection if the result has a null value and class is a set
+	 * @should return true when converting an arbitrary object to Boolean
+	 * @should return false when converting falsey values to Boolean
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T convert(CalculationResult result, Class<T> clazz) {
 		if (clazz == null)
 			throw new ConversionException("Please specify a class to which to convert the result");
+		
+		if (Boolean.class.equals(clazz)) {
+			return (T) Boolean.valueOf(isTrue(result));
+		}
 		
 		if (result == null || result.isEmpty()) {
 			Collection<T> coll = null;
@@ -90,4 +101,142 @@ public class ResultUtil {
 		
 		return CalculationUtil.cast(valueToConvert, clazz);
 	}
+	
+	/**
+	 * If you pass in a CalculationResult, this method will operate upon its getValue, otherwise it will operate on the
+	 * object itself.
+	 * 
+	 * "Truthy" is defined as "not falsey".
+	 * @see #isFalse(Object)
+	 * 
+	 * @param o
+	 * @return whether the given object is "truthy"
+	 */
+	public static boolean isTrue(Object o) {
+		return !isFalse(o);
+	}
+	
+	/**
+	 * If you pass in a CalculationResult, this method will operate upon its getValue, otherwise it will operate on the
+	 * object itself.
+	 * 
+	 * The following are treated as false:
+	 * <ul>
+	 * <li>null</li>
+	 * <li>empty collections/maps</li>
+	 * <li>the number 0</li>
+	 * <li>the empty string</li>
+	 * <li>numeric Obs whose value is 0</li>
+	 * <li>text Obs whose value is ""</li>
+	 * <li>coded Obs whose value is the GlobalProperty concept.false</li>
+	 * <li>boolean Obs whose value is false</li>
+	 * </ul>
+	 * * the number 0
+	 * * the empty string
+	 * * numeric Ob
+	 * 
+	 * @param o
+	 * @return whether the given object is "falsey"
+	 * @should return true for null
+	 * @should return true for an empty collection
+	 * @should return false for a non-empty collection
+	 * @should return true for an empty map
+	 * @should return false for a non-empty map
+	 * @should return true for an empty string
+	 * @should return false for a non-empty string
+	 * @should return true for the number 0
+	 * @should return false for a non-zero number
+	 * @should return true for a numeric Obs whose value is 0
+	 * @should return false for a numeric Obs whose value is not 0
+	 * @should return true for a text Obs whose value is the empty string
+	 * @should return false for a text Obs whose value is not the empty string
+	 * @should return true for a coded Obs whose value is the GlobalProperty concept.false
+	 * @should return false for a coded Obs whose value is not the GlobalProperty concept.false
+	 * @should return true for a boolean Obs whose value is false
+	 * @should return false for a boolean Obs whose value is true
+	 * @should return true for an empty ListResult
+	 * @should return false for non-empty ListResult
+	 * @should return true for an empty SimpleResult
+	 * @should return false for a non-empty SimpleResult
+	 */
+	public static boolean isFalse(Object o) {
+		if (o == null) {
+			return true;
+		} else if (o instanceof CalculationResult) {
+			CalculationResult cr = (CalculationResult) o;
+			return cr.isEmpty() || isFalse(cr.getValue());
+		} else if (o instanceof Boolean) {
+			return Boolean.FALSE.equals(o);
+		} else if (o instanceof String) {
+			return "".equals(o);
+		} else if (o instanceof Collection<?>) {
+			return ((Collection<?>) o).isEmpty();
+		} else if (o instanceof Map<?, ?>) {
+			return ((Map<?, ?>) o).isEmpty();
+		} else if (o instanceof Integer || o instanceof Long) {
+			return ((Number) o).longValue() == 0l;
+		} else if (o instanceof BigDecimal) {
+			return o.equals(BigDecimal.ZERO);
+		} else if (o instanceof BigInteger) {
+			return o.equals(BigInteger.ZERO);
+		} else if (o instanceof Number) {
+			// Double, Float, and anything else
+			return ((Number) o).doubleValue() == 0d;
+		} else if (o instanceof Obs) {
+			Obs obs = (Obs) o;
+			if (obs.getConcept().getDatatype().isNumeric()) {
+				return isFalse(obs.getValueNumeric());
+			} else if (obs.getConcept().getDatatype().isBoolean()) {
+				// ideally we'd want to use obs.getValueBoolean, but this isn't introduced until 1.7 and this module requires 1.6.5.
+				return isFalse(obs.getValueAsBoolean());
+			} else if (obs.getConcept().getDatatype().isText()) {
+				return isFalse(obs.getValueText());
+			} else if (obs.getConcept().getDatatype().isCoded()) {
+				if (OpenmrsConstants.OPENMRS_VERSION_SHORT.startsWith("1.6.")) {
+					// prior to 1.7 booleans were represented as numeric 0 or 1, so if this is 1.6.x, we treat all non-null coded obs as true
+					return obs.getValueCoded() == null;
+				} else {
+					// in 1.7+, there is a GlobalProperty for concept.false, and we should treat that as falsey
+					Boolean b = obs.getValueAsBoolean();
+					if (b == null) {
+						// the obs.getValueAsBoolean method returns null for any valueCoded that isn't explicitly True or False
+						return obs.getValueCoded() == null;
+					} else {
+						// if the value is explicitly the GP concept.true or concept.false, we'll get back a value here
+						return !b;
+					}
+				}
+			}
+		} else if (o instanceof Concept) {
+			// TODO handle this if we're on OpenMRS 1.7+ when ConceptService.getFalseConcept() was introduced 
+		}
+		return false;
+	}
+	
+	/**
+	 * Similar to {@link #isFalse(Object)}, but some false values (the number 0, the empty string, and coded
+	 * values of GP:concept.false) are <em>not</em> treated as empty.
+	 * 
+	 * @param o
+	 * @return for CalculationResults, delegates to {@link CalculationResult#isEmpty()}, otherwise returns true for null, or for empty Collections and Maps
+	 * @should return true for null
+	 * @should return true for empty collections
+	 * @should return true for empty maps
+	 * @should return false for non-empty collections
+	 * @should return false for non-empty maps
+	 * @should return false for plain objects
+	 */
+	public static boolean isEmpty(Object o) {
+		if (o == null) {
+			return true;
+		} else if (o instanceof CalculationResult) {
+			return ((CalculationResult) o).isEmpty();
+		} else if (o instanceof Collection<?>) {
+			return ((Collection<?>) o).isEmpty();
+		} else if (o instanceof Map<?, ?>) {
+			return ((Map<?, ?>) o).isEmpty();
+		}
+		return false;
+	}
+
 }
--- api/src/test/java/org/openmrs/calculation/result/ResultUtilTest.java
+++ api/src/test/java/org/openmrs/calculation/result/ResultUtilTest.java
@@ -13,7 +13,10 @@
  */
 package org.openmrs.calculation.result;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -21,6 +24,11 @@ import java.util.Set;
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.openmrs.Concept;
+import org.openmrs.ConceptDatatype;
+import org.openmrs.Obs;
+import org.openmrs.PatientProgram;
 import org.openmrs.test.Verifies;
 
 public class ResultUtilTest {
@@ -131,4 +139,321 @@ public class ResultUtilTest {
 		Assert.assertNotNull(set);
 		Assert.assertTrue(set.isEmpty());
 	}
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for null
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForNull() throws Exception {
+	    Assert.assertTrue(ResultUtil.isFalse(null));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for an empty collection
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForAnEmptyCollection() throws Exception {
+    	Assert.assertTrue(ResultUtil.isFalse(new ArrayList<String>()));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for a non-empty collection
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForANonemptyCollection() throws Exception {
+    	Assert.assertFalse(ResultUtil.isFalse(Collections.singleton("Not Empty")));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for an empty map
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForAnEmptyMap() throws Exception {
+    	Assert.assertTrue(ResultUtil.isFalse(new HashMap<String, Object>()));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for a non-empty map
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForANonemptyMap() throws Exception {
+    	Map<String, Object> map = new HashMap<String, Object>();
+    	map.put("not", "empty");
+    	Assert.assertFalse(ResultUtil.isFalse(map));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for an empty string
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForAnEmptyString() throws Exception {
+    	Assert.assertTrue(ResultUtil.isFalse(""));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for a non-empty string
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForANonemptyString() throws Exception {
+    	Assert.assertFalse(ResultUtil.isFalse("Not empty"));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for the number 0
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForTheNumber0() throws Exception {
+    	Assert.assertTrue(ResultUtil.isFalse(0));
+    	Assert.assertTrue(ResultUtil.isFalse(0d));
+    	Assert.assertTrue(ResultUtil.isFalse(BigDecimal.ZERO));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for a non-zero number
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForANonzeroNumber() throws Exception {
+    	Assert.assertFalse(ResultUtil.isFalse(-1));
+    	Assert.assertFalse(ResultUtil.isFalse(10d));
+    	Assert.assertFalse(ResultUtil.isFalse(BigDecimal.ONE));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for a numeric Obs whose value is 0
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForANumericObsWhoseValueIs0() throws Exception {
+    	Obs obs = new Obs();
+    	obs.setConcept(mockConcept(ConceptDatatype.NUMERIC_UUID));
+    	obs.setValueNumeric(0d);
+    	Assert.assertTrue(ResultUtil.isFalse(obs));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for a numeric Obs whose value is not 0
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForANumericObsWhoseValueIsNot0() throws Exception {
+    	Obs obs = new Obs();
+    	obs.setConcept(mockConcept(ConceptDatatype.NUMERIC_UUID));
+    	obs.setValueNumeric(-1d);
+    	Assert.assertFalse(ResultUtil.isFalse(obs));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for a text Obs whose value is the empty string
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForATextObsWhoseValueIsTheEmptyString() throws Exception {
+    	Obs obs = new Obs();
+    	obs.setConcept(mockConcept(ConceptDatatype.TEXT_UUID));
+    	obs.setValueText("");
+    	Assert.assertTrue(ResultUtil.isFalse(obs));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for a text Obs whose value is not the empty string
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForATextObsWhoseValueIsNotTheEmptyString() throws Exception {
+    	Obs obs = new Obs();
+    	obs.setConcept(mockConcept(ConceptDatatype.TEXT_UUID));
+    	obs.setValueText("not empty");
+    	Assert.assertFalse(ResultUtil.isFalse(obs));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for a coded Obs whose value is the GlobalProperty concept.false
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForACodedObsWhoseValueIsTheGlobalPropertyConceptfalse() throws Exception {
+    	// this test should be different for 1.7+ 
+    	Obs obs = Mockito.mock(Obs.class);
+    	Mockito.when(obs.getValueAsBoolean()).thenReturn(Boolean.FALSE);
+    	Mockito.when(obs.getConcept()).thenReturn(mockConcept(ConceptDatatype.CODED_UUID));
+    	Assert.assertTrue(ResultUtil.isFalse(obs));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for a coded Obs whose value is not the GlobalProperty concept.false
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForACodedObsWhoseValueIsNotTheGlobalPropertyConceptfalse() throws Exception {
+    	Obs obs = Mockito.mock(Obs.class);
+    	Mockito.when(obs.getValueCoded()).thenReturn(new Concept());
+    	// in 1.7 the test should do this instead on mocking getValueCoded
+    	// Mockito.when(obs.getValueAsBoolean()).thenReturn(null);
+    	Mockito.when(obs.getConcept()).thenReturn(mockConcept(ConceptDatatype.CODED_UUID));
+    	Assert.assertFalse(ResultUtil.isFalse(obs));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for a boolean Obs whose value is false
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForABooleanObsWhoseValueIsFalse() throws Exception {
+    	Obs obs = Mockito.mock(Obs.class);
+    	Mockito.when(obs.getValueAsBoolean()).thenReturn(Boolean.FALSE);
+    	Mockito.when(obs.getConcept()).thenReturn(mockConcept(ConceptDatatype.BOOLEAN_UUID));
+    	Assert.assertTrue(ResultUtil.isFalse(obs));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for a boolean Obs whose value is true
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForABooleanObsWhoseValueIsTrue() throws Exception {
+    	Obs obs = Mockito.mock(Obs.class);
+    	Mockito.when(obs.getValueAsBoolean()).thenReturn(Boolean.TRUE);
+    	Mockito.when(obs.getConcept()).thenReturn(mockConcept(ConceptDatatype.BOOLEAN_UUID));
+    	Assert.assertFalse(ResultUtil.isFalse(obs));
+    }
+    
+	/**
+     * Creates a mock concept with a mock concept datatype with the given uuid
+     * 
+     * @param numericUuid
+     * @return
+     */
+    private Concept mockConcept(String conceptDatatypeUuid) {
+    	ConceptDatatype dt = new ConceptDatatype();
+    	dt.setUuid(conceptDatatypeUuid);
+    	Concept mock = new Concept();
+    	mock.setDatatype(dt);
+    	return mock;
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for an empty ListResult
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForAnEmptyListResult() throws Exception {
+    	Assert.assertTrue(ResultUtil.isFalse(new ListResult()));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for non-empty ListResult
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForNonemptyListResult() throws Exception {
+    	ListResult r = new ListResult();
+    	r.add(new SimpleResult("not empty", null));
+    	Assert.assertFalse(ResultUtil.isFalse(r));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return true for an empty SimpleResult
+     */
+    @Test
+    public void isFalse_shouldReturnTrueForAnEmptySimpleResult() throws Exception {
+    	// it's bad practice to have a SimpleResult whose value is null, but we should still handle the case 
+    	Assert.assertTrue(ResultUtil.isFalse(new SimpleResult(null, null)));
+    }
+
+	/**
+     * @see ResultUtil#isFalse(Object)
+     * @verifies return false for a non-empty SimpleResult
+     */
+    @Test
+    public void isFalse_shouldReturnFalseForANonemptySimpleResult() throws Exception {
+    	Assert.assertFalse(ResultUtil.isFalse(new SimpleResult(new PatientProgram(), null)));
+    }
+
+	/**
+     * @see ResultUtil#isEmpty(Object)
+     * @verifies return true for null
+     */
+    @Test
+    public void isEmpty_shouldReturnTrueForNull() throws Exception {
+	    Assert.assertTrue(ResultUtil.isEmpty(null));
+    }
+
+	/**
+     * @see ResultUtil#isEmpty(Object)
+     * @verifies return true for empty collections
+     */
+    @Test
+    public void isEmpty_shouldReturnTrueForEmptyCollections() throws Exception {
+    	Assert.assertTrue(ResultUtil.isEmpty(Collections.EMPTY_LIST));
+    }
+
+	/**
+     * @see ResultUtil#isEmpty(Object)
+     * @verifies return true for empty maps
+     */
+    @Test
+    public void isEmpty_shouldReturnTrueForEmptyMaps() throws Exception {
+    	Assert.assertTrue(ResultUtil.isEmpty(Collections.EMPTY_MAP));
+    }
+
+	/**
+     * @see ResultUtil#isEmpty(Object)
+     * @verifies return false for non-empty collections
+     */
+    @Test
+    public void isEmpty_shouldReturnFalseForNonemptyCollections() throws Exception {
+    	Assert.assertFalse(ResultUtil.isEmpty(Collections.singleton("not empty")));
+    }
+
+	/**
+     * @see ResultUtil#isEmpty(Object)
+     * @verifies return false for non-empty maps
+     */
+    @Test
+    public void isEmpty_shouldReturnFalseForNonemptyMaps() throws Exception {
+    	Map<String, Object> map = new HashMap<String, Object>();
+    	map.put("not", "empty");
+    	Assert.assertFalse(ResultUtil.isEmpty(map));
+    }
+
+	/**
+     * @see ResultUtil#isEmpty(Object)
+     * @verifies return false for plain objects
+     */
+    @Test
+    public void isEmpty_shouldReturnFalseForPlainObjects() throws Exception {
+    	Assert.assertFalse(ResultUtil.isEmpty(-1));
+    }
+
+	/**
+     * @see ResultUtil#convert(CalculationResult,Class)
+     * @verifies return true when converting an arbitrary object to Boolean
+     */
+    @Test
+    public void convert_shouldReturnTrueWhenConvertingAnArbitraryObjectToBoolean() throws Exception {
+	    Assert.assertTrue(ResultUtil.convert(new SimpleResult(new PatientProgram(), null), Boolean.class));
+    }
+
+	/**
+     * @see ResultUtil#convert(CalculationResult,Class)
+     * @verifies return false when converting falsey values to Boolean
+     */
+    @Test
+    public void convert_shouldReturnFalseWhenConvertingFalseyValuesToBoolean() throws Exception {
+    	Object[] falsey = new Object[] { "", 0, 0d, Boolean.FALSE, null };
+    	for (Object f : falsey) {
+    		Assert.assertFalse(ResultUtil.convert(new SimpleResult(f, null), Boolean.class));
+    	}
+    }
+    
 }
